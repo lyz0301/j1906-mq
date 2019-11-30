6d91c1e51ba38ae1db74b6fa10a3a2303e96f92f
lyz0301 <1761569470@qq.com>
2019-11-30 10:06:05 +0800

    Initial commit

---
+++ j1906-rabbitmq/src/main/java/com/qf/j1906/rabbitmq/J1906RabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.qf.j1906.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J1906RabbitmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(J1906RabbitmqApplication.class, args);
+    }
+
+}
---
+++ j1906-rabbitmq/src/test/java/com/qf/j1906/rabbitmq/J1906RabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qf.j1906.rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J1906RabbitmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
